b6650e2de28f08ceac2b3d4c3749170b81942598
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-12-22 06:13:21 -0500

    Update suzyBot.py

--- src/suzyBot.py
+++ src/suzyBot.py
@@ -2,13 +2,13 @@ from utilities import Utilities
 
 class SuzyBot:
     @staticmethod
-    def interpHelp(string=""):
+    def interpHelp(string):
         if string == '--':
             return "**!!!!!!!!!هذا هو البادئة بوت تأخير**"
         elif string == '--help':
             return '*!!!!!!!!!!!!!!!!!!!!ما ، تحتاج إلى مساعدة ، لا مساعدة ، اللعنة عليك ، أنا سوزي*'
-        else:
-            return ".أنا عنصري."
+        elif string == '--exit':
+            Utilities.closeServer()
 
     @staticmethod
     def printStartupDetails(client):
