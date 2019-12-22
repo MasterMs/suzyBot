a371a463473a498290b793dee5e2b7848e98f326
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-12-22 05:58:26 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,31 +1,24 @@
 import os
 import discord 
 from dotenv import load_dotenv
+from utilities import Utilities
+from suzyBot import SuzyBot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 game = discord.Game("Beating David | --help")
 
-
-def botOut(string):
-    if string == '--':
-        return "**!!!!!!!!!هذا هو البادئة بوت تأخير**"
-    elif string == '--help':
-        return '*!!!!!!!!!!!!!!!!!!!!ما ، تحتاج إلى مساعدة ، لا مساعدة ، اللعنة عليك ، أنا سوزي*'
-    else:
-        return ".أنا عنصري."
-
 @client.event
 async def on_ready():
-    print(f'\nStartup Details\n###################\n{client.user.name} has connected to {client.guilds[0].name}\nping: {str(client.latency*100)}ms')
+    SuzyBot.printStartupDetails(client)    
     await client.change_presence(status=discord.Status.idle, activity=game)
 
 @client.event
 async def on_message(message):
     controlChars = ['-','!']
     if message.content[0:2] == '--':
-        await message.channel.send(botOut(message.content))
+        await message.channel.send(SuzyBot.interpHelp(message.content))
     
     elif (message.content[0] in controlChars) and (message.channel.name == 'general'):
         print(f'Delete message {message.content}')
