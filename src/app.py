197fcdadcd57616fdb7efa4385af8272a17f28ab
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-12-22 17:04:37 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import os
 import discord 
+import json
 from dotenv import load_dotenv
 from utilities import Utilities
 from suzyBot import SuzyBot
@@ -7,22 +8,28 @@ from suzyBot import SuzyBot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
+suzy = SuzyBot()
+               
 
 @client.event
 async def on_ready():
-    SuzyBot.printStartupDetails(client)    
+    SuzyBot.printStartupDetails(client)
+    suzy.readUsers()
     await client.change_presence(status=discord.Status.online, activity=discord.Game("Beating David | --help"))
 
 @client.event
 async def on_message(message):
     controlChars = ['-','!']
+    suzy.checkDel()
     if message.content[0:2] == '--':
-        await message.channel.send(f'<@{message.author.id}>{SuzyBot.interpHelp(message.content)}')
+        await message.channel.send(f'<@{message.author.id}>{suzy.interp(message.content)}')
         await message.delete()
     elif (message.content[0] in controlChars) and (message.channel.name == 'general'):
         print(f'Delete message {message.content}')
         await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention} ASSHOLE**')
         await message.delete()
+    elif message.content in suzy.users[message.author.id]['words']:
+            message.delete()
 
 
 if __name__ == "__main__":
