3da87b9bcdd49f1317b629d4f95a3110f4e7937e
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-12-22 05:50:03 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,20 +4,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-
 client = discord.Client()
+game = discord.Game("Beating David | --help")
+
+
+def botOut(string):
+    if string == '--':
+        return "**!!!!!!!!!هذا هو البادئة بوت تأخير**"
+    elif string == '--help':
+        return '*!!!!!!!!!!!!!!!!!!!!ما ، تحتاج إلى مساعدة ، لا مساعدة ، اللعنة عليك ، أنا سوزي*'
+    else:
+        return ".أنا عنصري."
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to the server')
+    print(f'\nStartup Details\n###################\n{client.user.name} has connected to {client.guilds[0].name}\nping: {str(client.latency*100)}ms')
+    await client.change_presence(status=discord.Status.idle, activity=game)
 
 @client.event
 async def on_message(message):
     controlChars = ['-','!']
-    if (message.content[0] not in controlChars) and (message.channel.name == 'general'):
+    if message.content[0:2] == '--':
+        await message.channel.send(botOut(message.content))
+    
+    elif (message.content[0] in controlChars) and (message.channel.name == 'general'):
         print(f'Delete message {message.content}')
         await message.delete()
         await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention} ASSHOLE**')
+    
 
-client.run(TOKEN) 
+if __name__ == "__main__":
+    client.run(TOKEN) 
+
