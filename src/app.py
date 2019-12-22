dc143fbe3c1f1fa0e61fd7d9b2830ffd5d333ed7
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-12-22 08:32:44 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,26 +7,23 @@ from suzyBot import SuzyBot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-game = discord.Game("Beating David | --help")
 
 @client.event
 async def on_ready():
     SuzyBot.printStartupDetails(client)    
-    await client.change_presence(status=discord.Status.idle, activity=game)
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("Beating David | --help"))
 
 @client.event
 async def on_message(message):
     controlChars = ['-','!']
-    print(message.content[0:2])
     if message.content[0:2] == '--':
-        await message.channel.send(SuzyBot.interpHelp(message.content))
+        await message.channel.send(f'<@{message.author.id}>{SuzyBot.interpHelp(message.content)}')
         await message.delete()
     elif (message.content[0] in controlChars) and (message.channel.name == 'general'):
         print(f'Delete message {message.content}')
         await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention} ASSHOLE**')
         await message.delete()
 
-    
 
 if __name__ == "__main__":
     client.run(TOKEN) 
