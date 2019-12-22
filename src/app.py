d6ff0cf22a5aa1894590750e1b760c60155b2fc8
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-12-22 06:13:19 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -17,13 +17,14 @@ async def on_ready():
 @client.event
 async def on_message(message):
     controlChars = ['-','!']
+    print(message.content[0:2])
     if message.content[0:2] == '--':
         await message.channel.send(SuzyBot.interpHelp(message.content))
+        await message.delete()
     elif (message.content[0] in controlChars) and (message.channel.name == 'general'):
         print(f'Delete message {message.content}')
         await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention} ASSHOLE**')
-        
-    await message.delete()
+        await message.delete()
 
     
 
