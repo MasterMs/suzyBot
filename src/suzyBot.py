35ba9431515125897f4ee1a74331a0cfc0888739
Marco Siciliano <59744751+sicilima@users.noreply.github.com>
2020-02-26 02:35:01 -0500

    Update suzyBot.py

--- src/suzyBot.py
+++ src/suzyBot.py
@@ -6,56 +6,33 @@ class SuzyBot(discord.Client):
     def __init__(self):
         super().__init__()
         self.userDict = {}
-        self.help = {
-            '*--help*': "Display Help for Bot",
-            '*--refresh*': "Update's user blacklist's from *userProfile.json*",
-            '*--hadar*': 'Make Suzy yell in arabic'
-            }
+        self.commands = {
+            '-play' : self.groovy_message
+        }
 
     async def on_ready(self):
         self.printStartupDetails()
         self.readUsers()
         await self.change_presence(status=discord.Status.online, activity=discord.Game("Beating David | --help"))
 
+    async def groovy_message(self, message):
+        print(f'Delete message {message.content}')
+        await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention} ASSHOLE**')
+        await message.delete()
 
     async def on_message(self, message):
         try:
-            controlChars = ['-','!']
-            if message.content[0:2] == '--':
-                await message.channel.send(f'<@{message.author.id}>{self.interp(message.content)}')
-                await message.delete()
-            elif (message.content[0] in controlChars) and (message.channel.name == 'general'):
-                print(f'Delete message {message.content}')
-                await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention} ASSHOLE**')
-                await message.delete()
-            elif message.content.lower() in self.userDict[message.author.id]['blacklist']:
-                await message.delete()
+            await self.commands[message.content](message)
         except KeyError:
             pass
         except IndexError:
             pass
     
     def readUsers(self):
-        with open('D:/suzyBot/src/userProfile.json', 'r') as userFile:
+        with open('G:/suzyBot/src/userProfile.json', 'r') as userFile:
             users = json.load(userFile)
             for user in users:
                 self.userDict[user['userId']] = user
-
-    def interp(self, string):
-        if string == '--':
-            return "**Suzy Bot Prefix**"
-        elif string == '--help':
-            helper = ''
-            for i in self.userDict:
-                helper.join([helper, i, self.userDict[i]])
-            return helper
-        elif string == '--refresh':
-            self.readUsers()
-            return " Blacklist refreshed"
-        elif string == '--exit':
-            Utilities.closeServer()
-        elif string == '--hadar':
-            return 'ديفيد أيها الأحمق الغبي ، لم أحبك أبداً ولن أفعل أبداً.'
 
     def printStartupDetails(self):
         Utilities.clear()
