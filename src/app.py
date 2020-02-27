3e815b7aa4205d0a1554b69d3813cfda8636e81b
Marco Siciliano <59744751+sicilima@users.noreply.github.com>
2020-02-26 19:56:18 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 import os
 import dotenv
+import pymongo
 from suzyBot import SuzyBot
 
 if __name__ == "__main__":
     try:
+        client = pymongo.MongoClient(f'mongodb+srv://admin:{os.getenv("DB_PASSWORD")}@suzy-db-f4pio.mongodb.net/test?retryWrites=true&w=majority')
+        db = client.test
         dotenv.load_dotenv()
         suzy = SuzyBot()
         suzy.run(os.getenv('DISCORD_TOKEN')) 
-    except:
-        print("Error Encountered, Program Now Exiting")
+    except Exception as e:
+        print(f"Error Encountered, Program Now Exiting\n{e}")
         exit()
