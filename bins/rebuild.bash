70d1376ac67ae73a466e86c72a14685a76cdd3d0
Marco Siciliano <59744751+sicilima@users.noreply.github.com>
2020-02-29 03:38:22 -0500

    Update rebuild.bash

--- bins/rebuild.bash
+++ bins/rebuild.bash
@@ -1,6 +1,6 @@
 docker stop suzy-bot
 docker rm suzy-bot
-docker build -t suzy-bot https://github.com/MasterMs/suzyBot.git 
+docker build -t https://github.com/MasterMs/suzyBot.git .
 docker run --name suzy-bot suzy-bot --env-file .env
 
 
