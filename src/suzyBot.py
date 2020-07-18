041a957476aac683f6ea5868e6e69268aa68d965
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2020-07-17 20:38:31 -0400

    Update suzyBot.py

--- src/suzyBot.py
+++ src/suzyBot.py
@@ -38,7 +38,7 @@ class SuzyBot(discord.Client):
         try:
             defn = self.words.meaning(word)
             if defn == {} or None:
-                defn = ud.define(word)
+                defn = ud.define(word)[0].definition
             return defn
         except Exception as e:
             self.errorInvoked(e)
