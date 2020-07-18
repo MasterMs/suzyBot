ad8bd69306fec830b9713c3d6cb78d2b558f7782
Marco <42436088+MasterMs@users.noreply.github.com>
2020-07-18 14:27:19 -0400

    Changes

--- src/suzyBot.py
+++ src/suzyBot.py
@@ -14,10 +14,10 @@ class SuzyBot(discord.Client):
     async def on_ready(self):
         try:
             await self.change_presence(status=discord.Status.online, activity=discord.Game("Beating David | --help"))
-            return 0
+            
         except Exception as e:
             self.errorInvoked(e)
-            return 1
+            
         
     async def on_message(self, message):
         try:
@@ -25,24 +25,22 @@ class SuzyBot(discord.Client):
                 await self.response(message, mode='g')
             elif any(s in message.content.lower() for s in self.db["SuzyData"]["Users"].find({"discordId": str(message.author.id).lower()})[0]["blacklist"]):
                 await self.response(message, mode='b')
-            return 0
+            
         except Exception as e:
-            self.errorInvoked(e)
-            return 1
+            self.errorInvoked(e)       
 
     async def response(self, message, mode=''):
         try:
             if mode == 'g':
                 await message.channel.send(f'<@{message.author.id}> **STOP SENDING BOT COMMANDS IN {message.channel.mention}**')
+                await message.delete()
             elif mode == 'b':
                 await message.channel.send(f'<@{message.author.id}> **Hey! you can\'t say that word!**')
-            else:
-                pass
-            await message.delete()
-            return 0
+                await message.delete()
+            
         except Exception as e:
             self.errorInvoked(e)
-            return 1
+            
 
     def errorInvoked(self, error):
         self.db["SuzyData"]["errors"].insert_one({
