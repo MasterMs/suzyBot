f805d33d4b729407bf5cb185a43bcb9e659a5859
Marco <42436088+MasterMs@users.noreply.github.com>
2020-07-18 14:37:36 -0400

    Revert "Merge branch 'master' of https://github.com/MasterMs/suzyBot"
    This reverts commit f840afd686227750d78eb20ada26a8332d2c9f42, reversing
    changes made to ad8bd69306fec830b9713c3d6cb78d2b558f7782.

--- bins/rebuild.bash
+++ bins/rebuild.bash
@@ -1,11 +1,6 @@
 docker stop suzy-bot
 docker rm suzy-bot
-if [[$1 == '-f']]
-then
-  docker build -t suzybot -v.0.0.1 $2
-else
-  docker build -t suzybot -v.0.0.1 https://github.com/MasterMs/suzyBot.git
-fi
+docker build -t suzybot-v.0.0.1 .
 docker run --name suzy-bot suzybot-v.0.0.1
 
 
--- src/app.py
+++ src/app.py
@@ -10,4 +10,3 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"Error Encountered, Program Now Exiting\n{e}")
         exit()
-        
