eee34abfb7f5b755264a8ab8d147bb60c0c6a91b
masterms <marcosiciliano1601@gmail.com>
2020-07-18 21:10:20 -0400

    Update embeds.py

--- src/embeds.py
+++ src/embeds.py
@@ -2,4 +2,6 @@ import discord
 
 class Groovy(discord.Embed):
     def __init__(self, **kwargs):
-        super().__init__(title='Suzybot', description='test')
+        super().__init__(**kwargs)
+        self.title = 'Test'
+        self.description = 'test'
