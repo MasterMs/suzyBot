9ba66c418676ffee989df517f2b4e1d296958fdc
masterms <marcosiciliano1601@gmail.com>
2020-11-02 20:17:44 -0500

    Fix groovt alert message

--- src/suzyBot.py
+++ src/suzyBot.py
@@ -50,7 +50,7 @@ class SuzyBot(discord.Client):
     async def response(self, message, mode=''):
         try:
             if mode == 'g':
-                await message.channel.send(f'<@{message.author.id}>! STOP SENDING GROOVY COMMANDS IN {message.channel.mention}**')
+                await message.channel.send(f'<@{message.author.id}>**STOP SENDING GROOVY COMMANDS IN {message.channel.mention}**!')
             elif mode == 'b':
                 await message.channel.send(f'<@{message.author.id}> **Hey! you can\'t say that word!**')
             await message.delete()
