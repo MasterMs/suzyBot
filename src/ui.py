dfe218bf2382ad8c4160f83a9621b1e7b73c668c
masterms <marcosiciliano1601@gmail.com>
2021-04-07 13:57:08 -0400

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -19,6 +19,8 @@ class Ui():
             await message.channel.send(self.tenor.random(f'{message.content.lower()[4:]}'))
         elif any(s in message.content.lower() for s in self.db["SuzyData"]["Users"].find({"discordId": str(message.author.id).lower()})[0]["blacklist"]):
             await message.channel.send(f'<@{message.author.id}> **Hey! you can\'t say that word!**')
+        elif message == "-members":
+            await message.channel.send(message.guild)
     
     def define(self, word):
         defn = f"***{word.capitalize()}***\n"
