253ce04b1a566c14fe6573bc32f4e869a64171a4
masterms <marcosiciliano1601@gmail.com>
2021-04-08 01:28:57 -0400

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -17,10 +17,6 @@ class Ui():
             await message.add_reaction(self.get_emoji(743673927691272243))
         elif message.content.lower()[0:3] == '-gif':
             await message.channel.send(self.tenor.random(f'{message.content.lower()[4:]}'))
-        elif message.content.lower() == "-members":
-            await message.channel.send(f"{message.guild.members[10]}")
-        # elif any(s in message.content.lower() for s in self.db["SuzyData"]["Users"].find({"discordId": str(message.author.id).lower()})[0]["blacklist"]):
-        #     await message.channel.send(f'<@{message.author.id}> **Hey! you can\'t say that word!**')
     
     def define(self, word):
         defn = f"***{word.capitalize()}***\n"
