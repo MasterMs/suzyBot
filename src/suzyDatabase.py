5279643f122a9309ef530bdecd10a82e3a16b4b3
Marco <42436088+MasterMs@users.noreply.github.com>
2021-04-11 22:34:17 -0400

    Update suzyDatabase.py

--- src/suzyDatabase.py
+++ src/suzyDatabase.py
@@ -1,9 +1,9 @@
 import pymongo
 from pymongo import mongo_client
 
-class SuzyDatabase(mongo_client):
-    def __init__(self) -> None:
-        super().__init__()
+class SuzyDatabase(pymongo.MongoClient):
+    def __init__(self, host=None, port=None, document_class=None, tz_aware=None, connect=None, type_registry=None, **kwargs):
+        super().__init__(host=host, port=port, document_class=document_class, tz_aware=tz_aware, connect=connect, type_registry=type_registry, **kwargs)
 
     def loadDatabase(self, guilds):
         for guild in guilds:        
