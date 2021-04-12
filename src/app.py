772d0a4f54d0ce103d84dc6610a7d1b7b418f560
Marco <42436088+MasterMs@users.noreply.github.com>
2021-04-11 22:12:45 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,10 @@
 import os
-import dotenv
 from suzyBot import SuzyBot
 
 if __name__ == "__main__":
     try:
-        dotenv.load_dotenv()
         suzy = SuzyBot()
         suzy.run(os.getenv('DISCORD_TOKEN'))
     except Exception as e:
-        print(f"Error Encountered, Program Now Exiting\n{e}")
+        print(f"{e}\nError Encountered, Program Now Exiting")
         exit()
