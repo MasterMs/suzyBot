30cc8eb2c10ee61ef2cba9d83b44efd8e7c62ef9
Marco <42436088+MasterMs@users.noreply.github.com>
2021-04-11 22:12:38 -0400

    Update suzyBot.py

--- src/suzyBot.py
+++ src/suzyBot.py
@@ -1,32 +1,17 @@
-import json
 import os
-import datetime
 import discord
-import dotenv
 from suzyDatabase import SuzyDatabase
 from ui import Ui
 
 class SuzyBot(discord.Client):
     def __init__(self):
         super().__init__()
-        self.db = SuzyDatabase(os.getenv("CONNECTION_STRING"))
+        self.db = SuzyDatabase(connect=os.getenv("DB_CONNECT"))
         self.ui = Ui()
 
     async def on_ready(self):
-        try:
-            await self.change_presence(status=discord.Status.online, activity=discord.Game("Beating David | --help"))
-        except Exception as e:
-            self.errorInvoked(e)
+        await self.change_presence(status=discord.Status.online, activity=discord.Game("Beating David | --help"))
 
     async def on_message(self, message):
-        try:
-            await self.ui.response(message=message)
-        except Exception as e:
-            self.errorInvoked(e)
-
-    def errorInvoked(self, e):
-        self.db["Errors"].insert_one([{
-                "date": datetime.datetime.utcnow(),
-                "error": e
-            }])
+        await self.ui.response(message=message)
 
