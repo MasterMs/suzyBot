2b53c4288ea951e40cd070c4d6bdd4d827f48ccc
Marco <42436088+MasterMs@users.noreply.github.com>
2021-04-11 22:50:35 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,9 +2,9 @@ import os
 from suzyBot import SuzyBot
 
 if __name__ == "__main__":
-    try:
-        suzy = SuzyBot()
-        suzy.run(os.getenv('DISCORD_TOKEN'))
-    except Exception as e:
-        print(f"{e}\nError Encountered, Program Now Exiting")
-        exit()
+    # try:
+    suzy = SuzyBot()
+    suzy.run(os.getenv('DISCORD_TOKEN'))
+    # except Exception as e:
+    #     print(f"{e}\nError Encountered, Program Now Exiting")
+    #     exit()
