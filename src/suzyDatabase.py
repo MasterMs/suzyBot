41495e640cee3c3a939b717c8a9c5b1a3ece567c
Marco <42436088+MasterMs@users.noreply.github.com>
2021-04-12 02:30:53 -0400

    Update suzyDatabase.py

--- src/suzyDatabase.py
+++ src/suzyDatabase.py
@@ -2,8 +2,8 @@ import pymongo
 from pymongo import mongo_client
 
 class SuzyDatabase(pymongo.MongoClient):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
+    def __init__(self, connect) -> None:
+        return super().__init__(connect=connect)
 
     def loadDatabase(self, guilds):
         for guild in guilds:        
